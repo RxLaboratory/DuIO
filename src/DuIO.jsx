1e295bf80f2d8cb0adce3a692e9c0633f70a29a8
Duduf <git@duduf.com>
2019-11-08 17:51:12 +0100

    initial commit

---
+++ src/DuIO.jsx
@@ -0,0 +1,9 @@
+#include "duik_required/Duik16_header.jsxinc"
+
+var standAlone = true;
+#include "duik_required/Duik16_io.jsxinc"
+ui_palette.alignChildren = ['fill','fill'];
+ui_buildIO( ui_palette );
+
+#include "duik_required/Duik16_footer.jsxinc"
+
