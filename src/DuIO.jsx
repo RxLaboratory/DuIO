bca1a5b3d30c2fbe7c92e0ccb4b5e31eab5153eb
Duduf <duduf@duduf.com>
2019-11-13 18:18:15 +0100

    1.0.0 release

--- src/DuIO.jsx
+++ src/DuIO.jsx
@@ -2,10 +2,10 @@
 
 var ui_tabs = DuAEF.DuScriptUI.addTabPanel(ui_contents);
 var ui_importTab = ui_tabs.addTab("Import",DuAEF.DuBinary.toFile(w25_import_l),"Import",DuAEF.DuBinary.toFile(w25_import_r));
-var ui_eTab = ui_tabs.addTab("Export",DuAEF.DuBinary.toFile(w25_exportfile_l),"Export",DuAEF.DuBinary.toFile(w25_exportfile_r));
+var ui_exportTab = ui_tabs.addTab("Export",DuAEF.DuBinary.toFile(w25_exportfile_l),"Export",DuAEF.DuBinary.toFile(w25_exportfile_r));
 
 #include 'duio_required/DuIO_import.jsxinc'
-//#include 'duio_required/DuIO_export.jsxinc'
+#include 'duio_required/DuIO_export.jsxinc'
 
 ui_tabs.setCurrentIndex(0);
 
