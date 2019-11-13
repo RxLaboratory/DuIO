1745c24b61342d398a16b55c6a5f90dab511d3c6
Duduf <duduf@duduf.com>
2019-11-13 17:03:58 +0100

    import UI implementation

--- src/DuIO.jsx
+++ src/DuIO.jsx
@@ -1,9 +1,12 @@
-#include "duik_required/Duik16_header.jsxinc"
+#include 'duio_required/DuIO_header.jsxinc'
 
-var standAlone = true;
-#include "duik_required/Duik16_io.jsxinc"
-ui_palette.alignChildren = ['fill','fill'];
-ui_buildIO( ui_palette );
+var ui_tabs = DuAEF.DuScriptUI.addTabPanel(ui_contents);
+var ui_importTab = ui_tabs.addTab("Import",DuAEF.DuBinary.toFile(w25_import_l),"Import",DuAEF.DuBinary.toFile(w25_import_r));
+var ui_eTab = ui_tabs.addTab("Export",DuAEF.DuBinary.toFile(w25_exportfile_l),"Export",DuAEF.DuBinary.toFile(w25_exportfile_r));
 
-#include "duik_required/Duik16_footer.jsxinc"
+#include 'duio_required/DuIO_import.jsxinc'
+//#include 'duio_required/DuIO_export.jsxinc'
 
+ui_tabs.setCurrentIndex(0);
+
+#include 'duio_required/DuIO_footer.jsxinc'
